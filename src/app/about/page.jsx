819debc85c7994d347d3d4123ec5223db46896b2
C0dewithawais <awaisalisabir4@gmail.com>
2025-05-16 12:11:49 +0500

    next

---
+++ src/app/about/page.jsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <div>This is About Page</div>;
+}
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,5 @@
+export default  function contact() {
+  return (
+    <div>this is a contact page</div>
+  )
+}
